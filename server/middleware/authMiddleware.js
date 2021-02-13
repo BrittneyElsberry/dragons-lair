729e766645e7ac75b336e9df1e130454aa2173a7
CoderBritt89 <72882552+CoderBritt89@users.noreply.github.com>
2021-02-13 14:05:46 -0700

    logout function doesn't work

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,7 +1,14 @@
 module.exports = {
-    usersOnly: (req, res)=>{
+    usersOnly: (req, res, next)=>{
         if(!req.session.user){
             return res.status(401).send('Please log in')
         } next()
+    },
+
+    adminOnly: (req, res, next)=>{
+        if (!req.session.user.isAdmin){
+            res.status(403).send('You are not an admin')
+        } next()
+
     }
 }
